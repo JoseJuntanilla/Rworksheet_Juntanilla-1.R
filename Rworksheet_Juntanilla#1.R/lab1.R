35aa27a8d55c96598a8a1eccac4a00cb1b979883
Jose Roland Juntanilla <joseroland.juntanilla@students.isatu.edu.ph>
2023-09-26 12:13:04 +0000

    updated commit

--- Rworksheet_Juntanilla#1.R/lab1.R
+++ Rworksheet_Juntanilla#1.R/lab1.R
@@ -24,3 +24,43 @@ Age_Vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
 
 length(Age_Vec)
 
+#7
+Age_Vec <- c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length(Age_Vec)
+Age_Vec*2
+
+#8.1
+Seq_first <- seq(1, 100)
+
+length(Seq_first)
+
+#8.2 Numbers from 20-60
+Seq_second <- seq(20, 60)
+
+
+#8.3 Mean of numbers from 20 to 60
+mean(Seq_second)
+
+#8.4 Sum of numbers from 51 to 91
+sum(51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91)
+
+#8.5 Integers from 1 to 1000
+Seq_third <- seq(1, 1000)
+
+max(Seq_third)
+
+#9. Print Vector
+
+filter(funtion(i) { all(i %% c(3,5,7) != 0),seq(100)
+
+
+#10.
+rev(Seq_first)
+
+
+#11.
+Natural_num <- seq(1, 24)
+Mult_of_3_or_5 <- Natural_num[which(numbers %% 3 == 0 | numbers %% 5 == 0)]
+
+#12
